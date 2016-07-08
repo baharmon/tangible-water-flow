387f1f82e23a775b3fa7df004a626d85488b2d44
baharmon <baharmon@ncsu.edu>
2016-07-08 14:43:35 -0400

    update

---
+++ webpage/build.sh
@@ -0,0 +1,8 @@
+./copy-common-files.py --dst-dir=build
+
+./build-slides.py --outdir=build \
+    --title="Tangible Landscape" \
+    --meta-description="Tangible Landscape" \
+    --meta-author="Brendan Harmon" \
+    --outfile=index.html \
+    index.html
